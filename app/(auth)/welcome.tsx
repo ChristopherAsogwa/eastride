bdf9e19996f11266ed1b614a2e1d9486e352c4e3
ChristopherAsogwa <christopherasogwa8@gmail.com>
2026-01-22 05:28:20 +0100

    Complete project setup, file & folder structure

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const Onboarding = () => {
   return (
     <SafeAreaView>
-      <Text>Onboarding</Text>
+      <Text>Onboarding Screen</Text>
     </SafeAreaView>
   );
 };
